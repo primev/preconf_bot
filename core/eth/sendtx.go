13bb1f37e6c59ddf8c2a7b9e358a9ab50c062e11
Evan-Kim2028 <ekcopersonal@gmail.com>
2024-10-28 18:16:03 -0400

    feat: set max fee to base fee

--- core/eth/sendtx.go
+++ core/eth/sendtx.go
@@ -42,12 +42,6 @@ func SelfETHTransfer(client *ethclient.Client, authAcct bb.AuthAcct, value *big.
 	
 	blockNumber := header.Number.Uint64()
 
-	// Set the max priority fee per gas to be 2 times the base fee
-	maxPriorityFee := new(big.Int).Mul(baseFee, big.NewInt(2))
-
-	// Set the max fee per gas to be 2 times the max priority fee
-	maxFeePerGas := new(big.Int).Mul(maxPriorityFee, big.NewInt(2))
-
 	// Get the chain ID (this does not work with the Titan RPC)
 	chainID, err := client.NetworkID(context.Background())
 	if err != nil {
@@ -60,7 +54,7 @@ func SelfETHTransfer(client *ethclient.Client, authAcct bb.AuthAcct, value *big.
 		To:        &authAcct.Address,
 		Value:     value,
 		Gas:       500_000,
-		GasFeeCap: maxFeePerGas,
+		GasFeeCap: baseFee,
 		GasTipCap: big.NewInt((0)),
 	})
 
