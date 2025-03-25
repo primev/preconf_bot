76ab673aac415228dafcbebbbfe4067bee8cd300
Shawn <44221603+shaspitz@users.noreply.github.com>
2025-03-25 14:48:29 -0700

    build mev-commit from v1.0-notif branch

--- bidder/entrypoint.sh
+++ bidder/entrypoint.sh
@@ -2,38 +2,12 @@
 
 set -e
 
-MEV_COMMIT_VERSION="${MEV_COMMIT_VERSION:-v1.1.0-rc2}"
 DOMAIN="${DOMAIN:-testnet.mev-commit.xyz}"
 KEYSTORE_DIR="./keystore"
-ARTIFACTS_BASE_URL="https://github.com/primev/mev-commit/releases/download"
 RPC_URL="${RPC_URL:-https://chainrpc.${DOMAIN}}"
 LOG_LEVEL="${LOG_LEVEL:-info}"
 LOG_FMT="${LOG_FMT:-text}"
-
 BINARY_PATH="/usr/local/bin/mev-commit"
-
-VERSION=${MEV_COMMIT_VERSION#v}
-
-ARCH=$(uname -m)
-if [ "${ARCH}" = "x86_64" ]; then
-  FILE="mev-commit_${VERSION}_Linux_x86_64.tar.gz"
-elif [ "${ARCH}" = "aarch64" ] || [ "${ARCH}" = "arm64" ]; then
-  FILE="mev-commit_${VERSION}_Linux_arm64.tar.gz"
-else
-  echo "Error: Unsupported architecture: ${ARCH}"
-  exit 1
-fi
-
-DOWNLOAD_URL="${ARTIFACTS_BASE_URL}/${MEV_COMMIT_VERSION}/${FILE}"
-TEMP_DIR=$(mktemp -d)
-
-curl -sL "${DOWNLOAD_URL}" -o "${TEMP_DIR}/${FILE}"
-tar -xzf "${TEMP_DIR}/${FILE}" -C "${TEMP_DIR}"
-
-sudo mv "${TEMP_DIR}/mev-commit" "${BINARY_PATH}"
-sudo chmod +x "${BINARY_PATH}"
-
-rm -rf "${TEMP_DIR}"
 
 BOOTNODE="/dnsaddr/bootnode.${DOMAIN}"
 CONTRACTS_URL="https://contracts.${DOMAIN}"
